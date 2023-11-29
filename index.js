341d1e9ad3241884aa66e921331e99d0c3ef943f
almeidabbm <almeidabatistam@gmail.com>
2023-11-29 15:09:15 +0000

    feat: add code from gcp samples

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { Storage } from "@google-cloud/storage";
-import { BigQuery } from "@google-cloud/bigquery";
+import { Logging } from "@google-cloud/logging";
+import { Datastore } from "@google-cloud/datastore";
 
 const storage = new Storage();
-const bigqueryClient = new BigQuery();
+const datastore = new Datastore();
 
 async function deleteFile() {
   const bucketName = "my-bucket";
@@ -13,9 +14,49 @@ async function deleteFile() {
   console.log(`gs://${bucketName}/${fileName} deleted`);
 }
 
-async function createDataset() {
-  const [dataset] = await bigqueryClient.createDataset("my_new_dataset");
-  console.log(`Dataset ${dataset.id} created.`);
+async function log(
+  projectId = "YOUR_PROJECT_ID", // Your Google Cloud Platform project ID
+  logName = "my-log" // The name of the log to write to
+) {
+  const logging = new Logging({ projectId });
+  const log = logging.log(logName);
+  const text = "Hello, world!";
+
+  const metadata = {
+    resource: { type: "global" },
+    severity: "INFO",
+  };
+
+  const entry = log.entry(metadata, text);
+
+  async function writeLog() {
+    await log.write(entry);
+    console.log(`Logged: ${text}`);
+  }
+  writeLog();
 }
 
-deleteFile().then(createDataset);
+async function createDataStoreTask() {
+  // The kind for the new entity
+  const kind = "Task";
+
+  // The name/ID for the new entity
+  const name = "do something";
+
+  // The Cloud Datastore key for the new entity
+  const taskKey = datastore.key([kind, name]);
+
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      description: "do something",
+    },
+  };
+
+  // Saves the entity
+  await datastore.save(task);
+  console.log(`Saved ${task.key.name}: ${task.data.description}`);
+}
+
+deleteFile().then(createDataStoreTask).then(log);
